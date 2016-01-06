cc7c3b6702f07b6ccb4f63985df465ae28e8ce5d
darighteous1 <tonislav.atanasov@gmail.com>
2016-01-06 22:05:26 +0200

    refactored project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    var containers = $(".content");
+    console.log(containers);
+    $(".navigation-button").each(function() {
+        this.addEventListener("click", function() {
+            var page = $(this).attr("data-link");
+            var id = "#" + page;
+            $(id).removeClass("hidden");
+
+            $(".content:not("+ id + ")").addClass("hidden");
+        });
+    });
+});
