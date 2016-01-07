026fc5d24ebce626ec14dcd916f1a3ecf37a624b
Ivaylo <atriumandvintrigol@gmail.com>
2016-01-07 10:44:01 +0200

    R.I.P. Defense

--- js/main.js
+++ js/main.js
@@ -1,32 +1,4 @@
 ﻿$(document).ready(function() {
-    setTimeout(function(){
-
-        $('#home-page').fadeOut();
-        var previous = document.getElementById('home-page');
-        var id = "#news-page";
-
-        $(previous).animate({
-            left: '-500%'
-        },
-        {
-            duration: 500,
-            easing: 'linear',
-            complete: function() {
-                $(previous).css('left', '200%');
-                $(previous).appendTo('#wrapper');
-            }
-        });
-
-        $(id).animate({
-            left: '0%'
-        }, 500);
-
-        $(previous).removeClass('content');
-        $(previous).addClass('hidden');
-        $(id).removeClass('hidden');
-        $(id).addClass('content');
-    }, 3000);
-    
     $('.navigation-button').click(function() {
         var caller = this.id;
         var elems = document.getElementsByClassName('content');
